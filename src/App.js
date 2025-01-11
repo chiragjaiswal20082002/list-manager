52dc427c5fc4a2ca0bdc6f5c965a0baa41446b00
chirag jaiswal <chiragjaiswal20082002@gmail.com>
2025-01-11 13:42:46 +0530

    hey

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Item List Manager</h1>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter item"
+        />
+        <button onClick={handleAddItem}>Add Item</button>
+      </div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
